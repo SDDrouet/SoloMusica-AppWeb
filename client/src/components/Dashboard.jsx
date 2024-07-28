09b5a3e4345c30a672215660e8421d85c6885a0e
sddrouet@espe.edu.ec <sddrouet@espe.edu.ec>
2024-07-28 15:33:00 -0500

    add in building img

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './Header'
+import { building } from '../assets/img'
 
 const Dashboard = () => {
   return (
@@ -8,6 +9,7 @@ const Dashboard = () => {
         <div className='w-[60%] my-2 bg-blue-500 p-4 flex items-center justify-evenly'>
           <h1 className='text-4xl text-black'>Administrador</h1>
         </div>
+        <img src={building} alt='building' className='w-1/4 rounded-2xl bg-slate-300 border-slate-800 border-4' />
     </div>
   )
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
+import { building } from '../assets/img'
 
 const Home = () => {
   return (
     <div className='w-full h-auto flex flex-col items-center justify-center bg-slate-400'>
       <Header />
-      <div className='h-[calc(100vh-64px)] bg-slate-400 flex justify-center items-center'>
-            <h1 className='text-4xl text-black'>Página de Inicio</h1>
+      <div className='h-[calc(100vh-64px)] bg-slate-400 flex flex-col justify-center items-center'>
+            <h1 className='text-4xl text-black mb-10'>Página de Inicio</h1>
+            <img src={building} alt='building' className='w-3/4 rounded-2xl bg-slate-300 border-slate-800 border-4' />
         </div>
     </div>
   )
